1def5bb8459c36d1fcc191432f76a10b90f4c269
JIHOON KIM <j5kim@ucsd.edu>
2017-05-15 13:46:12 -0700

    add provisioning script and its testrun script

---
+++ provision.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------
+# Author : Jihoon Kim 
+# Description: A provisioning script to install R and PLINK for 
+#              differentially private Transmission Disequilibrium Test (dpTDT)
+#              docker
+#-----------------------------------------------------------------------------
+# add CRAN (https://cran.r-project.org/) to sources.list
+export CRAN_URL=http://cran.stat.ucla.edu
+bash -c " echo -e '\n'deb ${CRAN_URL}/bin/linux/ubuntu  trusty/ >>  /etc/apt/sources.list "
+
+# add key to sign CRAN pacakges
+# The Ubuntu archives on CRAN are signed with the key of 
+#  "Michael Rutter <marutter@gmail.com>" with key ID E084DAB9. 
+#   reference  http://cran.r-project.org/bin/linux/ubuntu/
+apt-key adv --keyserver keyserver.ubuntu.com  --recv-keys E084DAB9
+
+# update
+apt-get update -y
+
+# add specfic PPA
+apt-get install -y python-software-properties 
+add-apt-repository -y ppa:marutter/rdev
+
+# update
+apt-get update -y
+
+# upgrade
+apt-get upgrade -y
+
+# install R base version
+apt-get install -y r-base
+
+# install R packages 
+apt-get install -y r-base-dev
+
+# install PLINK
+mkdir -p /opt
+cd /opt
+wget http://pngu.mgh.harvard.edu/~purcell/plink/dist/plink-1.07-x86_64.zip
+unzip plink-1.07-x86_64.zip
+cd plink-1.07-x86_64
+ln -s /opt/plink-1.07-x86_64/plink /usr/local/bin/plink 
---
+++ testrun.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------
+#  File name : testrun.sh
+#  Author    : Jihoon Kim
+#  Email : j5kim@ucsd.edu
+#  Description : A test run script for dpTDT docker 
+#                 argument 1:  s
+#-----------------------------------------------------------------------------
+
+# an output from the test run
+TESTRUN_OUTPUT=/opt/dpTDT/dpTDT_output.txt
+
+# testrun dpTDT with example input
+cd /opt/dpTDT
+rm -f $TESTRUN_OUTPUT
+bash /opt/dpTDT/dpTDT.sh --prefix=sample --N=25 --K=3 --eps=3
+
+# "Gold Standard" desired md5 sum value of the final output
+DESIRED_OUTPUT_MD5="6492b873163746dc3159db61e68f8772"
+
+# return 0 if the test run output is equal to the desired output
+#        non-zero otherwise
+FOUND=`md5sum ${TESTRUN_OUTPUT} | awk '{ print $1 }'| grep -c "${DESIRED_OUTPUT_MD5}"`
+EXITCODE=$((1 - $FOUND))
+exit $EXITCODE
